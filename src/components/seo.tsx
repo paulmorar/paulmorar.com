7e9d104a76a0ec03a9ffea53682def859067d0a3
paulmorar <paulcornelmorar@gmail.com>
2019-11-04 01:18:03 +0100

    Convert seo component

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -10,7 +10,14 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+interface Props {
+  description?: string
+  lang?: string
+  meta?: []
+  title: string
+}
+
+const SEO = ({ description = "", lang = "en", meta = [], title }: Props) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -70,19 +77,6 @@ function SEO({ description, lang, meta, title }) {
       ].concat(meta)}
     />
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export default SEO
